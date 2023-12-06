dfcd0fafba8c0a67fcbbfed626a2c7065817d814
Tomislav Najdovski <tomsaso@gmail.com>
2023-12-06 18:31:41 +0100

    small fix to permissons

--- sat/permissons.py
+++ sat/permissons.py
@@ -5,5 +5,7 @@ class IsOwner(permissions.BasePermission):
     """
     Allow only owners access to the object
     """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
     def has_object_permission(self, request, view, obj):
         return obj.owner ==request.user
